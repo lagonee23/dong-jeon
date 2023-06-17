06ff7133e0209d525489cad2e7242a368d3244ce
Keonhui Seo <lagonee23@gmail.com>
2023-06-17 13:21:11 +0900

    first commit

--- __init__.py
+++ __init__.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, world!"
+    return render_template('index.html')
+
+@app.route('/<int:status>.html/')
+def status(status):
+    if status in (401, 404, 500):
+        return render_template(f'{status}.html')
+    
+@app.route('/twitter.html/')
+def twitter():
+    return render_template('twitter.html')
 
 
 if __name__ == '__main__':
